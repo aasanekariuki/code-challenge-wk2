3712e647368a7df87c58fef8a5b91d88417df1f6
aasane kariuki <franciskariki053@gmail.com>
2024-03-31 13:39:03 +0300

    code-challenge

--- primenumbers.js
+++ primenumbers.js
@@ -1,4 +1,4 @@
-// implementing the function isPrime
+// Function to check if a number is prime
 function isPrime(number) {
     for(let i = 2; i < number; i++) {
       if(number % i === 0) {
@@ -7,9 +7,9 @@ function isPrime(number) {
     }
     return number !== 1;
   }
-  
+  // Filter the array to get only prime numbers using isPrime function
   let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   let primes = array.filter(isPrime);
-  
+  // Output the original array and the prime numbers
   console.log("array: " + array);
   console.log("primes: " + primes);
